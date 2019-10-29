3bb7cedc825c37716141c2ca9cc6dc528d8856b1
smitajivass <56814705+smitajivass@users.noreply.github.com>
2019-10-29 10:39:28 +0530

    Create table.py

---
+++ table.py
@@ -0,0 +1,16 @@
+## 1. Read the integer number n and m and print out n times tables until the mth position.
+## Eg n=2, m=5
+
+
+n=2
+m=5
+for i in range (1,m+1):
+ print (n,'x',i,"=",n*i)
+
+
+
+
+
+
+
+
